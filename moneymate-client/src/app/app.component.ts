9801c0f3b50b1962a5cf6aa9bae59d294580c21d
Ayush9535 <ghodkeayush22@gmail.com>
2024-12-16 16:07:41 +0530

    Login and Register page frontend added along with authguard. Routes Created for all pages

--- moneymate-client/src/app/app.component.ts
+++ moneymate-client/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'moneymate-client';
+export class AppComponent implements OnInit {
+  title = 'MoneyMate';
+  showNavbar: boolean = true;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path;
+        this.showNavbar = !(currentRoute === 'login' || currentRoute === 'register');
+      }
+    });
+  }
 }
--- moneymate-client/src/app/services/auth.service.ts
+++ moneymate-client/src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:5000/api/auth'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  login(email: string, password: string): Observable<any> {
+    const body = { email, password };
+    return this.http.post(`${this.apiUrl}/login`, body);
+  }
+
+  signup(name: string, email: string, password: string): Observable<any> {
+    const body = { name, email, password };
+    return this.http.post(`${this.apiUrl}/signup`, body);
+  }
 }
